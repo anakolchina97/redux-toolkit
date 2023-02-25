76cc9bc7bb4c3295eaaf49a02c461ec281bc9ea7
Annie <anniehartmann97@gmail.com>
2023-02-25 17:19:03 +0300

    :tada: Lesson done!

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,23 @@ import "./App.css";
 import { useState } from "react";
 import TodoList from "./components/TodoList";
 import InputField from "./components/InputField";
+import { useDispatch } from "react-redux";
+
+import { addTodo } from "./store/todoSlice";
 
 function App() {
-  const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
+  const dispatch = useDispatch();
 
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        },
-      ]);
-      setText("");
-    }
-  };
-
-  const removeTodo = (todoId) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
-  };
-
-  const toggleTodoComplete = (todoId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== todoId) return todo;
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      })
-    );
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
   };
 
   return (
     <div className="App">
-      <InputField text={text} setText={setText} addTodo={addTodo} />
-      <TodoList
-        todos={todos}
-        removeTodo={removeTodo}
-        toggleTodoComplete={toggleTodoComplete}
-      />
+      <InputField text={text} setText={setText} addTodo={addTask} />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeTodo, toggleTodoComplete } from "../store/todoSlice";
 
-const TodoItem = ({ id, completed, text, removeTodo, toggleTodoComplete }) => {
+const TodoItem = ({ id, completed, text }) => {
+  const dispatch = useDispatch();
   return (
     <li>
       <input
         type={"checkbox"}
         checked={completed}
-        onChange={() => toggleTodoComplete(id)}
+        onChange={() => dispatch(toggleTodoComplete({ id }))}
       />
       <span>{text}</span>
-      <span className="del" onClick={() => removeTodo(id)}>
+      <span className="del" onClick={() => dispatch(removeTodo({ id }))}>
         &times;
       </span>
     </li>
